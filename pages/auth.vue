af8447d47f7f015604b9c3efc9e290849b583c1e
HW1337 <kskobelin@ya.ru>
2024-04-06 19:25:05 +0700

    added card component and some fixes

--- pages/auth.vue
+++ pages/auth.vue
@@ -1,18 +1,30 @@
 <template>
-    <Auth
-      :supabaseClient="supabaseClient"
-      :appearance="{
-        theme: ThemeSupa
-      }"
-    />
-  </template>
-  
-  <script setup lang="ts">
-    import { ThemeSupa } from '@supabase/auth-ui-shared'
-    import { Auth } from '@nuxtbase/auth-ui-vue'
-  
-    const supabaseClient = useSupabaseClient();
-    definePageMeta({
-        layouts: 'home'
-    })
-  </script>
+  <div class="v-full min-h-screen flex items-center">
+    <div class="max-w-[800px] w-full mx-auto">
+      <Card>
+        <CardTitle>
+          <div class="text-center py-4">
+            Welcome!
+          </div>
+        </CardTitle>
+        <CardContent>
+          <Auth :supabaseClient="supabaseClient" :appearance="{
+            theme: ThemeSupa
+          }" :providers="['github', 'google']"
+          redirectTo=""/>
+        </CardContent>
+      </Card>
+    </div>
+  </div>
+
+</template>
+
+<script setup lang="ts">
+import { ThemeSupa } from '@supabase/auth-ui-shared'
+import { Auth } from '@nuxtbase/auth-ui-vue'
+
+const supabaseClient = useSupabaseClient();
+definePageMeta({
+  layouts: 'home'
+})
+</script>
