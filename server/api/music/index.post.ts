6f0875a171b06d00927f5619d12ef8eca394f3c1
HW1337 <kskobelin@ya.ru>
2024-05-24 16:46:08 +0700

    added limit check

--- server/api/music/index.post.ts
+++ server/api/music/index.post.ts
@@ -1,5 +1,6 @@
 import Replicate from "replicate";
-import { protectedRoute } from '~/server/utils';
+import { protectedRoute, incrementApiLimit, checkApiLimit } from '~/server/utils';
+import { User } from "~/server/types";
 
 const config = useRuntimeConfig();
 
@@ -10,7 +11,7 @@ const replicate = new Replicate({
 export default defineEventHandler(async (event) => {
 
     await protectedRoute(event);
-
+    const user = event.context.user as User
     const { prompt } = await readBody(event);
     if (!replicate.auth) {
         throw createError({
@@ -26,12 +27,23 @@ export default defineEventHandler(async (event) => {
 
     }
 
+    const freeTrial = await checkApiLimit(user.id);
+    const isPro = await checkSubscription(user.id)
+
+    if (!freeTrial && !isPro) {
+        throw createError({
+            statusCode: 403,
+            statusMessage: 'Free trial has expired.',
+        })
+
+    }
     const model = "riffusion/riffusion:8cf61ea6c56afd61d8f5b9ffd14d7c216c0a93844ce2d82ac1c9ecc9c7f24e05"
     const response = await replicate.run(model, {
         input: {
             prompt_a: prompt
         }
     })
+    await incrementApiLimit(user.id);
 
     return response
 
