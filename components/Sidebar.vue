96a0f5ab448fbdf7c27ba1436fc86a03b67b6c1c
HW1337 <kskobelin@ya.ru>
2024-06-10 23:50:36 +0300

    added counter ui

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -18,11 +18,36 @@
         </NuxtLink>
       </div>
     </div>
+    <div v-if="!isPro" class="px-3 border-t border-b">
+      <div class="bg-white/10 border-0">
+        <div class="py-6 px-2">
+          <div class="text-center text-sm mb-4 space-y-2">
+            <p>{{ user ? user.apiCount : 0 }} / {{ MAX_COUNT }} Бесплатных генераций</p>
+            <Progress v-model="progress" class="w-full" />
+          </div>
+          <Button class="w-full" variant="premium">
+            Апгрейд
+            <Icon name="lucide:zap" class="w-4 h-4 ml-2 fill-white" />
+          </Button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 const currentRoute = useRoute()
+const { data: user, pending } = await useFetch('/api/user', {
+  method: 'get',
+  key: 'userData',
+});
+
+const progress = computed(() => {
+  if (user.value && pending.value) {
+    return (user.value.apiCount / MAX_COUNT) * 100;
+  }
+  return 0;
+});
 </script>
 
 <style scoped></style>
